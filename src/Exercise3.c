190010be69aa3d5557c5cee7b74c33bc647415cb
giabao18 <phamgiabao1862001@gmail.com>
2021-10-18 17:14:06 +0700

    Ex3

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -15,22 +15,56 @@ Ex:
 void Ex3(int in_arr[], int n)
 {
 	// Your codes here
-	int max = 1, len = 1, lc;
+	int max1 = 1, len1 = 1, lc;
 	for (int i = 1; i < n - 1; i++)
 	{
 		if (in_arr[i] > in_arr[i - 1])
-			len++;
+			len1++;
 		else
-		{
-			if (max < len)
+		{ // 2 5 3 4 8 9 7 6 10
+			if (max1 < len1)
 			{
-				max = len;
-				lc = i;
+				max1 = len1;
+				// lc = i;
 			}
-			len = 1;
+			len1 = 1;
 		}
-		if (max < len)
-			max = len;
+		if (max1 < len1)
+		{
+			max1 = len1;
+			lc = i;
+		}
+	}
+	printf(" Increasing");
+	for (int i = lc - max1 + 1; i < lc + 1; i++)
+	{
+		printf(" %d", in_arr[i]);
+	}
+	int lc2;
+	int max2 = 1, len2 = 1;
+	for (int i = 1; i < n - 1; i++)
+	{
+		if (in_arr[i] < in_arr[i - 1])
+			len2++;
+		else
+		{ // 2 5 3 4 8 9 7 6 10
+			if (max2 < len2)
+			{
+				max2 = len2;
+				// lc = i;
+			}
+			len2 = 1;
+		}
+		if (max2 < len2)
+		{
+			max2 = len2;
+			lc2 = i;
+		}
+	}
+	printf(" Decreasing");
+	for (int i = lc2 - max2 + 1; i < lc2 + 1; i++)
+	{
+		printf(" %d", in_arr[i]);
 	}
 }
 
